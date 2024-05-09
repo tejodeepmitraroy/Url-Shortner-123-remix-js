f4407ed75a558393f06219f94f0fb79316029585
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2024-05-09 23:40:53 +0530

    feat: update with netlify-cli

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
@@ -6,5 +8,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [remix(),netlifyPlugin(), tsconfigPaths()],
 });
